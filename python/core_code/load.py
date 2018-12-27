cc8e2489b599b59e3efdb2164d351e43ef54121b
沈正圆 <914138410@qq.com>
2018-12-27 11:28:41 +0800

    add

--- python/core_code/load.py
+++ python/core_code/load.py
@@ -2,17 +2,19 @@ import os
 import scipy.io as sio
 import numpy as np
 from PIL import Image
+import h5py
+from tqdm import tqdm
 
 
-def load_testdata(digit_label):
+def get_testdata(digit_label):
     data_ind = []
     data = []
-    i_label = 1
+    i_label = 0
     for label in digit_label:
         filename = os.path.join("../testdata", "test" + str(label) + ".mat")
-        file_mat = sio.loadmat(filename)['D']
+        file_mat = sio.loadmat(filename)["D"]
         num_digits = file_mat.shape[0]
-        for i in range(num_digits):
+        for i in tqdm(range(num_digits)):
             ind_correct = 0
             data_ind.append([label, i_label, ind_correct])
             data.append(file_mat[i])
@@ -20,8 +22,22 @@ def load_testdata(digit_label):
     return np.array(data_ind), np.array(data)
 
 
-def load_filterdata(digit_label, type):
-    pass
+def get_filterdata(digit_label, type):
+    data_ind = []
+    data = []
+    i_label = 0
+    for label in digit_label:
+        filename = os.path.join("../filterdata", type + str(label) + ".mat")
+        file_mat = h5py.File(filename)["D_filtered"]
+        file_mat = np.array(np.transpose(file_mat))
+        num_digits = file_mat.shape[0]
+        for i in tqdm(range(num_digits)):
+            ind_correct = 0
+            data_ind.append([label, i_label, ind_correct])
+            data.append(file_mat[i])
+            i_label += 1
+    np.random.shuffle(data_ind)
+    return np.array(data_ind), np.array(data)
 
 
 # shape(784,)
@@ -32,5 +48,7 @@ def show_img(arr):
 
 
 if __name__ == '__main__':
-    ind, d = load_testdata([1, 2])
-    show_img(d[0])
+    test_ind, test_data = get_testdata([1, 2, 4])
+    # show_img(test_data[0])
+
+    filter_data, filter_data = get_filterdata([i for i in range(10)], "digit")
