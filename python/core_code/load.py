61bfffa77e171a4b6a605d871c9dccffb0d12d04
沈正圆 <914138410@qq.com>
2018-12-27 14:06:08 +0800

    add

--- python/core_code/load.py
+++ python/core_code/load.py
@@ -3,10 +3,10 @@ import scipy.io as sio
 import numpy as np
 from PIL import Image
 import h5py
-from tqdm import tqdm
 
 
 def get_testdata(digit_label):
+    print("start load test data:" + str(digit_label))
     data_ind = []
     data = []
     i_label = 0
@@ -14,16 +14,18 @@ def get_testdata(digit_label):
         filename = os.path.join("../testdata", "test" + str(label) + ".mat")
         file_mat = sio.loadmat(filename)["D"]
         num_digits = file_mat.shape[0]
-        for i in tqdm(range(num_digits)):
+        for i in range(num_digits):
             ind_correct = 0
             data_ind.append([label, i_label, ind_correct])
             data.append(file_mat[i])
             i_label += 1
+    print("successfully loaded test data,sum = " + str(i_label))
     return np.array(data_ind), np.array(data)
 
 
 # type:"digit" or "test"
 def get_filterdata(digit_label, type):
+    print("start load filter data:" + str(digit_label))
     data_ind = []
     data = []
     i_label = 0
@@ -32,12 +34,13 @@ def get_filterdata(digit_label, type):
         file_mat = h5py.File(filename)["D_filtered"]
         file_mat = np.array(np.transpose(file_mat))
         num_digits = file_mat.shape[0]
-        for i in tqdm(range(num_digits)):
+        for i in range(num_digits):
             ind_correct = 0
             data_ind.append([label, i_label, ind_correct])
             data.append(file_mat[i])
             i_label += 1
     np.random.shuffle(data_ind)
+    print("successfully loaded filter data,sum = " + str(i_label))
     return np.array(data_ind), np.array(data)
 
 
@@ -49,7 +52,7 @@ def show_img(arr):
 
 
 if __name__ == '__main__':
-    test_ind, test_data = get_testdata([1, 2, 4])
+    test_ind, test_data = get_testdata([i for i in range(10)])
     # show_img(test_data[0])
 
-    filter_data, filter_data = get_filterdata([i for i in range(10)], "digit")
+    filter_ind, filter_data = get_filterdata([i for i in range(10)], "digit")
