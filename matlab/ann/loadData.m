c6a9387a0e5bb59deff82a840a88658d8926f5a6
沈正圆 <914138410@qq.com>
2019-01-04 21:11:13 +0800

    add

--- matlab/ann/loadData.m
+++ matlab/ann/loadData.m
@@ -3,3 +3,12 @@ test_imgs = loadMNISTImages("../mnist_data/t10k-images-idx3-ubyte");
 test_labels = loadMNISTLabels("../mnist_data/t10k-labels-idx1-ubyte");
 train_imgs = loadMNISTImages("../mnist_data/train-images-idx3-ubyte");
 trian_labels = loadMNISTLabels("../mnist_data/train-labels-idx1-ubyte");
+
+figure;
+%产生随机数
+perm = randperm(60000,20);
+for i = 1:20
+    %绘制子图
+    subplot(4,5,i);
+    imshow(train_imgs(:,:,perm(i)));
+end
--- matlab/ann/loadMNISTImages.m
+++ matlab/ann/loadMNISTImages.m
@@ -11,8 +11,8 @@ numCols = fread(fp, 1, 'int32', 0, 'ieee-be');
 images = fread(fp, inf, 'unsigned char'); 
 images = reshape(images, numCols, numRows, numImages); 
 images = permute(images,[2 1 3]); fclose(fp); 
-% Reshape to #pixels x #examples 
-images = reshape(images, size(images, 3), size(images, 1) * size(images, 2));
-% Convert to double and rescale to [0,1] 
-images = double(images) / 255; 
+%Reshape to #pixels x #examples 
+%images = reshape(images, size(images, 3), size(images, 1) * size(images, 2));
+%Convert to double and rescale to [0,1] 
+%images = double(images) / 255; 
 end
