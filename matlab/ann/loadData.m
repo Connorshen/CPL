49ef5f359ecd3dff2edeb63c6150073fd027f758
沈正圆 <914138410@qq.com>
2019-01-05 14:50:33 +0800

    add

--- matlab/ann/loadData.m
+++ matlab/ann/loadData.m
@@ -1,4 +1,4 @@
-clearvars
+function [train_imgs,trian_labels,test_imgs,test_labels] = loadData()
 test_imgs = loadMNISTImages("../mnist_data/t10k-images-idx3-ubyte");
 test_labels = loadMNISTLabels("../mnist_data/t10k-labels-idx1-ubyte");
 train_imgs = loadMNISTImages("../mnist_data/train-images-idx3-ubyte");
