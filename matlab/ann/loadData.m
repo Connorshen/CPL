03edc90000fae6c3f36abecb4fd99ad2d17d6033
沈正圆 <914138410@qq.com>
2019-01-05 21:29:02 +0800

    add

--- matlab/ann/loadData.m
+++ matlab/ann/loadData.m
@@ -3,4 +3,5 @@ test_imgs = loadMNISTImages("../mnist_data/t10k-images-idx3-ubyte");
 test_labels = loadMNISTLabels("../mnist_data/t10k-labels-idx1-ubyte");
 train_imgs = loadMNISTImages("../mnist_data/train-images-idx3-ubyte");
 trian_labels = loadMNISTLabels("../mnist_data/train-labels-idx1-ubyte");
+end
 
