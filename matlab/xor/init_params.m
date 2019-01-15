2f2d535b01a901757f8148614de964969d6534ed
比尔丶盖子 <914138410@qq.com>
2019-01-15 14:21:05 +0800

    init xor

---
+++ matlab/xor/init_params.m
@@ -0,0 +1,9 @@
+classdef init_params
+    properties(Constant)
+        epoch = 1;
+        batch_size = 4;
+        learning_rate = 0.1;
+        
+        mehod = train_method.type_bp;
+    end
+end
---
+++ matlab/xor/train_method.m
@@ -0,0 +1,6 @@
+classdef train_method
+    properties(Constant)
+        type_bp = 'method_bp';
+        type_cpl = 'method_cpl';
+    end
+end
