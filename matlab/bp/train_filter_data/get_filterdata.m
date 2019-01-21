e6775a6a92d648ee6230007b7ac598dcfb40aed5
比尔丶盖子 <914138410@qq.com>
2019-01-21 13:17:37 +0800

    add

--- matlab/bp/train_filter_data/get_filterdata.m
+++ matlab/bp/train_filter_data/get_filterdata.m
@@ -7,13 +7,15 @@ for label = digit_label
     filename = ['../../filterdata/' type int2str(label)];
     load(filename);
     
+    label_onehot = zeros(10,1);
+    label_onehot(label+1) = 1;
+    
     for n = 1:size(D_filtered,1)
-        labels(end+1) = label;
+        labels(end+1,:) = label_onehot;
     end
     imgs = [imgs;D_filtered];
 end
 
-labels = labels';
 num_digit_data = randperm(length(labels));
 labels = labels(num_digit_data, :);
 imgs = imgs(num_digit_data, :);
