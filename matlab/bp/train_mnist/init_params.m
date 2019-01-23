80985e234ad9d076c175a30507d6d74fe3840996
比尔丶盖子 <914138410@qq.com>
2019-01-23 10:15:06 +0800

    add

--- matlab/bp/train_mnist/init_params.m
+++ matlab/bp/train_mnist/init_params.m
@@ -9,17 +9,17 @@ classdef init_params
     end
     methods
         function params = init_params()
-            params.epoch = 1;
-            params.batch_size = 500;
+            params.epoch = 1000;
+            params.batch_size = 200;
             params.learning_rate = 0.01;
             % it be used to compute momentum,beta is the discount of the before dw. eq:v_dW=βv_dW+(1-β)dW 
             params.beta = 0.9;
             % neuron of each layer
-            neurons = [784 20 20 10];
+            neurons = [784 20 10];
             % activation of each layer
-            acts = [activation.none,activation.sigmoid,activation.sigmoid,activation.softmax];
+            acts = [activation.none,activation.sigmoid,activation.softmax];
             % type of each layer
-            t_layer = [layer.dense,layer.dense,layer.dense,layer.dense];
+            t_layer = [layer.dense,layer.dense,layer.dense];
             %num of layer
             params.n_layer = length(neurons);
             % combine layers to struct
