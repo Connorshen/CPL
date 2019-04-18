d840613533231e10e2ec3c9abc3a957d38ce6d3a
比尔盖子先生 <914138410@qq.com>
2019-04-18 20:02:49 +0800

    add

---
+++ python/cnn/test.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+@Time    : 4/18/19 7:42 PM
+@Author  : 比尔丶盖子
+@Email   : 914138410@qq.com
+"""
+from keras.models import Sequential
+from keras.layers import MaxPool2D,Conv2D
+
+model = Sequential()
+model.add(Conv2D())
